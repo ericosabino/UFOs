6916cac108c6c3bee6b494c9a0962a7d4a7e5454
Érico Sabino <ericosabino@Ericos-MacBook-Pro.local>
2021-11-04 09:20:59 -0400

    Table created

--- app.js
+++ app.js
@@ -5,5 +5,20 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
+
+    // First, clear out any existing data
     tbody.html("");
-  }
+  
+data.array.forEach((dataRow) => {
+    // Append a row to the table body
+    let row = tbody.append("tr");
+
+    // Loop through each field in the dataRow and add
+    // each value as a table cell (td)
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.appemd("td");
+        cell.text(val)
+        }
+    );
+});
+}
